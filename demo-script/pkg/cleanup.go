a769a28c22d4cd8570da3cfb1b6e7760bea8a484
Andoni A <14891798+andoniaf@users.noreply.github.com>
2023-05-24 15:34:39 +0200

    fix: Cleanup script

--- demo-script/pkg/cleanup.go
+++ demo-script/pkg/cleanup.go
@@ -7,7 +7,7 @@ import (
 
 func Cleanup(ctx *cli.Context) error {
 	_ = Ensure(
-		"docker-compose -f docker-compose.yml down &&",
+		"docker-compose -f docker-compose.yml down",
 		"docker-compose -f opentelemetry-demo/docker-compose.yml down",
 	)
 	return nil
